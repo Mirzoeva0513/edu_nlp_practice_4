3eee9f69fda81f15729d955d30fed5d7a75b8057
Mirzoeva0513 <145575731+Mirzoeva0513@users.noreply.github.com>
2023-12-27 20:50:43 +0300

    Update task_zoo.py

--- task_zoo.py
+++ task_zoo.py
@@ -11,4 +11,27 @@ from abc import ABC
 
 ### ваше решение: ###
 class Animal(ABC):
-
+class Animal(ABC):
+    ["22:44"]
+from abc import ABC, abstractmethod
+ 
+class Animal(ABC):
+    def __init__(self, name="Unknown", info="Generic animal"):
+        self.name = name
+        self.info = info
+ 
+class Zebra(Animal):
+    def __init__(self, name, info, age=0):
+        super().__init__(name, info)
+        self.age = age
+ 
+class Dolphin(Animal):
+    def __init__(self, name, info, age=0):
+        super().__init__(name, info)
+        self.age = age
+    ["22:45"]
+zebra = Zebra("Zebra", "Striped mammal", age=5)
+dolphin = Dolphin("Dolphin", "Smart marine mammal", age=10)
+ 
+print(zebra.name, zebra.info, zebra.age)  # Вывод: Zebra Striped mammal 5
+print(dolphin.name, dolphin.info, dolphin.age)  # Вывод: Dolphin Smart marine mammal 10
